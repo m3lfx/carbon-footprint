61465861510bc5d92427cec104925aa78dd31b2b
rommel dalisay <rommelmdalisay@gmail.com>
2023-11-04 09:47:48 +0800

    radio button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,41 @@
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
 import Container from '@mui/material/Container';
 import './App.css'
 import ElectricityChart from './ElectricityChart'
-import { Box } from '@mui/system';
+import { Box, FormControlLabel, Radio, RadioGroup, FormLabel, FormControl } from '@mui/material';
 import GasChart from './GasChart';
 
 
 function App() {
+  const [value, setValue] = useState('elec');
 
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
   return (
     <Container maxWidth="lg">
-      <Box sx={{ bgcolor: '#cfe8fc', }} >
+      {value === 'elec' ? <Box sx={{ bgcolor: '#cfe8fc', }} >
         <ElectricityChart />
+      </Box> :
+        <Box sx={{ bgcolor: '#FCE5A4', }} >
+          <GasChart />
+        </Box>}
+      <Box  >
+        <FormControl>
+          <FormLabel id="demo-controlled-radio-buttons-group">Gender</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-controlled-radio-buttons-group"
+            name="controlled-radio-buttons-group"
+            value={value}
+            onChange={handleChange}
+            row={true}
+          >
+            <FormControlLabel value="elec" control={<Radio />} label="Electricity" />
+            <FormControlLabel value="gas" control={<Radio />} label="Gas" />
+          </RadioGroup>
+        </FormControl>
       </Box>
-      <Box sx={{ bgcolor: '#FCE5A4', }} >
-        <GasChart />
-      </Box>
-
     </Container>
   )
 }
